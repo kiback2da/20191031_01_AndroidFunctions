18e13d753d7b711d6252c9ba96f71805e0f4d651
kiback2da <kiback2da@steconsulting.co.kr>
2019-10-31 22:08:44 +0900

    첨부된 데이터 받아서 활용하기. (MainActivity)

--- app/src/main/java/com/tjoeun/a20191031_01_androidfunctions/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191031_01_androidfunctions/MainActivity.kt
@@ -2,6 +2,7 @@ package com.tjoeun.a20191031_01_androidfunctions
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +11,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var receivedName = intent.getStringExtra("user")
+        var receivedNumber = intent.getStringExtra("number")
+
+        Log.d("로그",receivedName)
+        Log.d("로그",receivedNumber)
+
+        /*
         mainName.text = intent.getStringExtra("user")
-        mainNum.text = intent.getStringExtra("number")+"번 회원"
+        mainNum.text = intent.getStringExtra("number")+"번 회원" */
+        mainName.text = receivedName
+        mainNum.text = "${receivedNumber} 번 회원"
     }
 }
