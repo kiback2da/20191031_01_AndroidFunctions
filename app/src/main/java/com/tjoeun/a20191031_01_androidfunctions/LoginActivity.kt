77a458a4c1d2ba6fc94cea5d999cbbb7115051e1
kiback2da <kiback2da@steconsulting.co.kr>
2019-10-31 20:51:06 +0900

    로그인 화면 초안 구성

--- app/src/main/java/com/tjoeun/a20191031_01_androidfunctions/LoginActivity.kt
+++ app/src/main/java/com/tjoeun/a20191031_01_androidfunctions/LoginActivity.kt
@@ -2,11 +2,16 @@ package com.tjoeun.a20191031_01_androidfunctions
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        loginBtn.setOnClickListener {
+            var intent
+        }
     }
 }
